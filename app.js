e62423b077c1301642391f7a58dedc54cb9157ea
HEEJU KIM <kljuyt107@gmail.com>
2020-01-14 11:43:07 +0900

    init_Server

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors())
+app.use(cookieParser());
+app.use(
+    session({
+        secret: process.env.COOKIE_SECRET,
+        resave: false,
+        cookie: {
+            httpOnly: true,
+            secure: false
+        },
+        saveUninitialized: true
+    })
+);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Founded');
+    err.status = 404;
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+   res.status(err.status||500).send('ERROR!')
+})
+
+app.listen(4000, () => {
+    console.log(`server listen on 4000`)
+})
