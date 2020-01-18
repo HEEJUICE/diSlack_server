dc086a9e5b8ddfeb31bb8637aa400621f8730be5
Dongin Kim <miknignod@gmail.com>
2020-01-18 22:11:12 +0900

    Added channelThread and Updated directThread

--- Routes/directThread.js
+++ Routes/directThread.js
@@ -6,11 +6,12 @@ const {
   Room,
   User,
 } = require("../models");
+const { isLoggedIn } = require("./middlewares");
 
 const router = express.Router();
 
 // /:code(workspace)/message/direct/:id(room)/:id(message)/list
-router.get("/list", async (req, res, next) => {
+router.get("/list", isLoggedIn, async (req, res, next) => {
   // 이전 미들웨어에서 저장한 정보를 변수에 저장한다
   const { code, room_id, msgId, user } = req;
 
@@ -44,7 +45,7 @@ router.get("/list", async (req, res, next) => {
 });
 
 // /:code(workspace)/message/direct/:id(room)/:id(message)
-router.post("/", async (req, res, next) => {
+router.post("/", isLoggedIn, async (req, res, next) => {
   // 이전 미들웨어에서 저장한 정보를 변수에 저장한다
   const { code, room_id, msgId, user } = req;
   const { reply } = req.body;
