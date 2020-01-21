47d8ed0f34d50eeb361f17a2cdfaf2db44b639b5
Hiz <kljuyt107@gmail.com>
2020-01-21 14:28:43 +0900

    Fixed socket files

--- socket/chat.js
+++ socket/chat.js
@@ -1,12 +1,12 @@
 module.exports = chat => {
   chat.on("connection", socket => {
     console.log("chat 네임스페이스 접속!");
-    const req = socket.request;
-    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    // const req = socket.request;
+    // const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
-    const {
-      headers: { referer },
-    } = req;
+    // const {
+    //   headers: { referer },
+    // } = req;
 
     socket.on("disconnect", () => {
       console.log("chat 네임스페이스 접속 해제!");
